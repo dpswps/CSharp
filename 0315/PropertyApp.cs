9c6a55ae4d36129f798f50a99b2454fbee33e1dc
dpswps <39237863+dpswps@users.noreply.github.com>
2021-03-15 10:34:14 +0900

    Add files via upload

---
+++ 0315/PropertyApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PropertyApp //패키지 개념
+{
+    class PropertyClass {
+        private int privateValue;
+        public int PrivateValue {
+            get { return privateValue; }
+            set { privateValue = value; }
+        }
+        public void Printvalue() {
+            Console.WriteLine("Hidden Value = " + privateValue);
+        }
+    }
+    class PropertyApp { 
+        static void Main(string[] args) {
+            int n;
+            PropertyClass obj = new PropertyClass();
+            obj.PrivateValue = 100;
+            obj.Printvalue();
+            n = obj.PrivateValue;
+            Console.WriteLine("Value = " + n);
+        }
+    }
+}
